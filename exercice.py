a621d6c608b95d913851fbe6d44ed7c71cae9ec8
Jacob Taylor <jacobtaylor27@users.noreply.github.com>
2021-03-29 16:16:55 -0400

    Update exercice.py

--- exercice.py
+++ exercice.py
@@ -12,14 +12,14 @@ def linear_values() -> np.ndarray:
 
 
 def coordinate_conversion(cartesian_coordinates: np.ndarray) -> np.ndarray:
-    return np.array([])
+    return np.array([(np.sqrt(x**2 + y**2), np.arctan2(y, x)) for x,y in cartesian_coordinates])
 
 
 def find_closest_index(values: np.ndarray, number: float) -> int:
-    return 0
+    return np.absolute(values - number).argmin()
 
 
 if __name__ == '__main__':
     # TODO: Appelez vos fonctions ici
-
-    pass
+    print(coordinate_conversion(np.array([(15,30), (11,15), (8,12)])))
+    print(find_closest_index(np.array([18,10,15,16]), 10.5))
